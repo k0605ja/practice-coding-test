edb279b34aeb4fc9da2cadad9823ef716fe203ff
k0605ja <k0605ja2@naver.com>
2023-03-17 16:53:47 +0900

    [Add] 한 줄에 두 정수 입력 받고 합 구하기

---
+++ 2588.py
@@ -0,0 +1,18 @@
+# 문제
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 A, 둘째 줄에 B가 주어진다. (0<A, B<10)
+
+# 출력
+# 첫째 줄에 A+B를 출력한다.
+
+
+
+a = int(input())
+b = input()
+
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0])) 
+print(a*int(b))
